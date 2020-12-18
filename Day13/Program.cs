deb97715149138437ab880f7beca8be5a68c82c2
edouard-chevalier <edouard-chevalier@users.noreply.github.com>
2020-12-18 10:09:11 +0100

    day12

---
+++ Day13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day13
+{
+    class Program
+    {
+        private const string input = @"";
+        private const string test = @"";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            exo1();
+        }
+
+        static void exo1()
+        {
+
+        }
+    }
+}

2b9d06ad379a0884df37e312fa94e993fc870872
edouard chevalier <edouard.chevalier@techmydata.net>
2020-12-19 14:38:42 +0100

    day15

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Linq;
 
 namespace Day13
 {
     class Program
     {
-        private const string input = @"";
-        private const string test = @"";
+        private const string input = @"1003240
+19,x,x,x,x,x,x,x,x,41,x,x,x,37,x,x,x,x,x,787,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,x,x,x,x,x,x,23,x,x,x,x,x,29,x,571,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,17
+";
+        private const string test = @"939
+7,13,x,x,59,x,31,19";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -14,7 +18,22 @@ namespace Day13
 
         static void exo1()
         {
-
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            int target = Int32.Parse(lines[0]);
+            int[] bus = lines[1].Split(',', StringSplitOptions.RemoveEmptyEntries).Where(s => s != "x")
+                .Select(Int32.Parse).ToArray();
+            int min = Int32.MaxValue;
+            int busID = -1;
+            foreach (var bu in bus)
+            {
+                int wait = ((target / bu + 1) * bu) - target;
+                if (wait < min)
+                {
+                    busID = bu;
+                    min = wait;
+                }
+            }
+            Console.WriteLine( busID * min);
         }
     }
 }
---
+++ Day15/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Day15
+{
+    class Program
+    {
+        private const string input = @"1,12,0,20,8,16";
+        private const string test = @"0,3,6";
+        private const string test1 = @"1,3,2";
+        private const string test2 = @"2,1,3";
+        private const string test3 = @"1,2,3";
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            exo1();
+        }
+
+        private static void exo1()
+        {
+            var nums = input.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToList();
+
+            var ages = nums.Take( nums.Count - 1).Select((i, v) => (i, v)).ToDictionary((tuple => tuple.i), tuple => tuple.v);
+
+            var lastSpoken = nums.Last();
+            for (int i = nums.Count; i < 30000000; i++)
+            {
+                int spoken = 0;
+                if (ages.TryGetValue(lastSpoken, out var pos)){
+                    spoken = i - 1 - pos;
+                }
+                
+                ages[lastSpoken] = i - 1;
+                lastSpoken = spoken;
+                if (i % 10000 == 0)
+                {
+                    Console.WriteLine($"{i}: {spoken}");
+                }
+            }
+            Console.WriteLine(lastSpoken);
+        }
+    }
+}

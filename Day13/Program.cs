e7ceaa38d58a43907111ad3b79f2cfba5758d0f0
edouard chevalier <edouard.chevalier@techmydata.net>
2020-12-25 15:38:02 +0100

    finish 2020

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -13,7 +13,7 @@ namespace Day13
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            exo1();
+            exo2();
         }
 
         static void exo1()
@@ -34,6 +34,47 @@ namespace Day13
                 }
             }
             Console.WriteLine( busID * min);
+        }
+        
+        static long modInverse(long a, long n) 
+        {
+            long i = n, v = 0, d = 1;
+            while (a>0) {
+                long t = i/a, x = a;
+                a = i % x;
+                i = x;
+                x = d;
+                d = v - t*x;
+                v = x;
+            }
+            v %= n;
+            if (v<0) v = (v+n)%n;
+            return v;
+        }
+        
+        static void exo2()
+        {
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            int target = Int32.Parse(lines[0]);
+            int[] bus = lines[1].Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s == "x" ? -1 : Int32.Parse(s)).ToArray();
+            long ts = 0;
+            long bftot = 1;
+
+            for (int i = 0; i < bus.Length; i++)
+            {
+                if (bus[i] < 0) continue;
+                int bf = bus[i];
+
+                // (bftot * n) % bftot = ( bf - (ts + i) % bf) % bf;
+                long inv = modInverse(bftot, bf);
+                long res = (bf - (ts + i) % bf) % bf;
+                long n = (inv * res) % bf;
+                ts += bftot * n;
+                bftot *= bf;
+            }
+            
+            Console.WriteLine( ts);
         }
     }
 }
---
+++ Day25/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace Day25
+{
+    class Program
+    {
+        private const string input = @"10441485
+1004920
+";
+        private const string test = @"5764801
+17807724";
+        static void Main(string[] args)
+        {
+            exo1();
+            
+        }
+
+        static long Transform(long subjectNumber, int loopSize, long res = 1)
+        {
+            for (int i = 0; i< loopSize; i++)
+            {
+                res *= subjectNumber;
+                res %= 20201227;
+            }
+
+            return res;
+        }
+
+        static int  BruteForce(long publicKey )
+        {
+            long res = 1;
+            for (int loopSize = 1; loopSize < 100000000; loopSize++)
+            {
+                res = Transform(7, 1, res);
+                if (res == publicKey)
+                {
+                    return loopSize;
+                }
+            }
+
+            throw new Exception();
+        }
+        static void exo1()
+        {
+            var vals = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse)
+                .ToArray();
+
+            long cardPublicKey = vals[0];
+            int cardLoopSize = BruteForce(cardPublicKey);
+
+            Console.WriteLine(cardLoopSize);
+            long doorPublicKey = vals[1];
+            int doorLoopSize = BruteForce(doorPublicKey);
+            Console.WriteLine(doorLoopSize);
+
+            Console.WriteLine(Transform(doorPublicKey, cardLoopSize));
+            Console.WriteLine(Transform(cardPublicKey, doorLoopSize));
+
+        }
+    }
+}
